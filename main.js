482805f6c783cda8a5d5fe1b0631a97d482ee2be
박종한 <exuberant94@naver.com>
2020-09-21 20:47:58 +0900

    Fourth Commit

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
 (function (window, document) {
   'use strict';
-
-  const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
-  const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
-
-  $toggleBtn.addEventListener('click', function () {
-    toggleElements();
-  });
-
-  window.addEventListener('resize', function () {
-    if (window.innerWidth > 1024) {
-      // Off Toggle elements
-      offElements();
-    }
-  });
-
-  function toggleElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.toggle('on');
-    });
-  }
-
-  function offElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.remove('on');
-    });
-  }
-
   $('#login').click(() => {
     $('#id01').css({ display: 'block' });
   });
 
   $('#cancel').click(() => {
     $('#id01').css({ display: 'none' });
+  });
+
+  $('#signup_cancel').click(() => {
+    $(location).attr('href', '/index.html');
   });
 })(window, document);
